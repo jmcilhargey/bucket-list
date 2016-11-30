35514c82495b1eb5cf0856ae26b7eb0d355a571a
jmcilhargey <jmcilhargey@gmail.com>
2016-11-29 23:48:40 -0800

    Supporting config and package files initial upload. Webpack bundling with babel transpile for es6 and jsx

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require("path");
+
+var HTMLWebpackPlugin = require("html-webpack-plugin");
+var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+  template: __dirname + "/client/source/index.html",
+  filename: "index.html",
+  inject: "body"
+});
+
+module.exports = {
+  entry: path.join(__dirname, "./client/source/app.js"),
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        loader: "style!css"
+      }
+    ]
+  },
+  output: {
+    path: path.join(__dirname, "./client/build"),
+    filename: "bundle.js"
+  },
+  plugins: [HTMLWebpackPluginConfig]
+};
