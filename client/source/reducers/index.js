9d7e22170f0aec79b7830314343cc7f0ae822afe
jmcilhargey <jmcilhargey@gmail.com>
2016-12-09 22:01:13 -0800

    Made reducer cases for registration and login actions

--- client/source/reducers/index.js
+++ client/source/reducers/index.js
@@ -1,10 +1,13 @@
 "use strict";
 
 import { combineReducers } from "redux";
+import { reducer as reduxFormReducer } from "redux-form";
 
 const newPinBox = (state = {
+    counter: 0,
     isBox: false,
-    previewUrl: ""
+    previewUrl: "",
+    isCreating: false
   }, action) => {
 
   switch(action.type) {
@@ -15,7 +18,10 @@ const newPinBox = (state = {
       return Object.assign({}, state, { isBox: false });
       break;
     case "PREVIEW_IMAGE":
-      return Object.assign({}, state, { url: action.url });
+      return Object.assign({}, state, { previewUrl: action.previewUrl });
+      break;
+    case "CREATE_PIN":
+      return Object.assign({}, state, { isCreating: true, isBox: false });
       break;
     default:
       return state;
@@ -24,7 +30,7 @@ const newPinBox = (state = {
 
 const pinsView = (state = {
   isFetching: false,
-  data: null,
+  pinData: null,
   receivedAt: null
 }, action) => {
 
@@ -33,7 +39,35 @@ const pinsView = (state = {
       return Object.assign({}, state, { isFetching: true });
       break;
     case "RECEIVE_PINS":
-      return Object.assign({}, state, { isFetching: false, data: action.data, receivedAt: action.receivedAt });
+      return Object.assign({}, state, { isFetching: false, pinData: action.data, receivedAt: action.receivedAt });
+      break;
+    default:
+      return state;
+  }
+}
+
+const registerUser = (state = {
+  isRegistering: false
+}, action) => {
+  switch(action.type) {
+    case "REGISTER_USER":
+      return Object.assign({}, state, { isRegistering: true });
+      break;
+    default:
+      return state;
+  }
+}
+
+const loginUser = (state = {
+  isLogin: false,
+  userData: null
+}, action) => {
+  switch(action.type) {
+    case "LOGIN_USER":
+      return Object.assign({}, state, { isLogin: true });
+      break;
+    case "RECEIVE_USER":
+      return Object.assign({}, state, { isLogin: false, userData: action.data });
       break;
     default:
       return state;
@@ -42,7 +76,10 @@ const pinsView = (state = {
 
 const rootReducer = combineReducers({
   newPinBox,
-  pinsView
+  pinsView,
+  registerUser,
+  loginUser,
+  form: reduxFormReducer
 });
 
 export default rootReducer;
