4a2396599a1be0ee32e5e23244545355237221a4
jmcilhargey <jmcilhargey@gmail.com>
2016-12-09 21:52:31 -0800

    Created actions and thunks for posting new user and new pins

--- client/source/actions/index.js
+++ client/source/actions/index.js
@@ -2,6 +2,23 @@
 
 import fetch from "isomorphic-fetch";
 
+export const fetchPins = () => (dispatch) => {
+  dispatch(requestPins());
+  return fetch("api/pins", {
+    method: "GET",
+    headers: {
+      "Accept": "application/json"
+    },
+    credentials: "same-origin"
+  }).then(response => response.json())
+    .then(json => {
+      dispatch(receivePins(json));
+    })
+    .catch(error => {
+      dispatch(receivePins(json));
+    });
+}
+
 export const showBox = () => {
   return { type: "SHOW_BOX" };
 }
@@ -11,15 +28,21 @@ export const hideBox = () => {
 }
 
 export const previewImage = (url) => {
-  return { type: "PREVIEW_IMAGE", previewUrl: url };
+  return {
+    type: "PREVIEW_IMAGE",
+    previewUrl: url
+  };
 }
 
 export const showPinDetail = (id) => {
-  return { type: "SHOW_PIN_DETAIL", pinId: id };
+  return {
+    type: "SHOW_PIN_DETAIL",
+    pinId: id
+  };
 }
 
 export const requestPins = () => {
-  return { type: "REQUEST_PINS", isFetching: true };
+  return { type: "REQUEST_PINS" };
 }
 
 export const receivePins = (json) => {
@@ -31,34 +54,85 @@ export const receivePins = (json) => {
   }
 }
 
+export const createPin = () => {
+  return { type: "CREATE_PIN" }
+}
+
+export const sendPinData = (data) => (dispatch, getState) => {
+  dispatch(createPin());
+  const token = getState().loginUser.userData.token;
+  return fetch("/api/pin", {
+    method: "POST",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/x-www-form-urlencoded",
+      "X-Access-Token": token
+    },
+    credentials: "same-origin",
+    body: `image=${ data.image }&title=${ data.title }&event=${ data.event }&address=${ data.address }&time=${ data.time }`
+  }).then(response => response.json())
+    .then(json => {
+      dispatch(receivePins(json));
+    })
+    .catch(error => {
+      dispatch(receivePins(error));
+    });
+}
+
 export const registerUser = () => {
-
+  return {
+    type: "REGISTER_USER",
+    isRegistering: true
+  }
 }
 
-export const loginUser = () => {
-
-}
-
-export const shouldFetchPins = () => (dispatch, getState) => {
-  console.log("hello");
-}
-
-export const fetchPins = () => (dispatch) => {
-
-  dispatch(requestPins());
-
-  return fetch("/api/pins", {
-    method: "GET",
+export const sendUserData = (data) => (dispatch) => {
+  dispatch(registerUser());
+  return fetch("/api/register", {
+    method: "POST",
     headers: {
       "Accept": "application/json",
       "Content-Type": "application/x-www-form-urlencoded"
     },
+    credentials: "same-origin",
+    body: `username=${ data.username }&email=${ data.email }&password=${ data.password }&confirm=${ data.confirm }`
+  }).then(response => response.json())
+    .then(json => {
+      dispatch(receiveUser(json));
+    })
+    .catch(error => {
+      dispatch(receiveUser(error));
+    });
+}
+
+export const receiveUser = (json) => {
+  return {
+    type: "RECEIVE_USER",
+    data: json
+  }
+}
+
+export const loginUser = () => {
+  return {
+    type: "REGISTER_USER",
+    isRegistering: true
+  }
+}
+
+export const sendLoginData = (data) => (dispatch) => {
+  dispatch(loginUser());
+  return fetch("/api/login", {
+    method: "GET",
+    headers: {
+      "Accept": "application/json",
+      "Authorization": "Basic " + btoa(`${ data.username }:${ data.password }`)
+    },
     credentials: "same-origin"
   }).then(response => response.json())
     .then(json => {
-      console.log(json);
+      dispatch(receiveUser(json));
     })
     .catch(error => {
-      console.log(error);
+      dispatch(receiveUser(error));
     });
 }
